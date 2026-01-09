0c96ad97b398cabf121df844da40a725378ad39d
Chaitanya Bhagwat <chaitanya@squarewaves.be>
2026-01-09 14:29:17 +0100

    chore: untrack .next build cache directory

--- apps/web/.next/dev/static/chunks/apps_web_src_app_lists_page_tsx_df443c1b._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/apps_web_src_de2c5606._.js",
-  "static/chunks/node_modules_1cab3cb8._.js"
-],
-    source: "dynamic"
-});
